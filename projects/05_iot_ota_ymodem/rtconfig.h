2d8cce2ec3b47f278dce8b799ba98a654ed62350
StackYuan <yuanjyjyj@outlook.com>
2024-03-27 19:14:23 +0800

    【修复】修改 05_iot_ota_ymodem 例程，补齐缺少的可执行文件。

--- projects/05_iot_ota_ymodem/rtconfig.h
+++ projects/05_iot_ota_ymodem/rtconfig.h
@@ -116,31 +116,6 @@
 #define RT_SFUD_USING_SFDP
 #define RT_SFUD_USING_FLASH_INFO_TABLE
 #define RT_SFUD_SPI_MAX_HZ 50000000
-#define RT_USING_WIFI
-#define RT_WLAN_DEVICE_STA_NAME "wlan0"
-#define RT_WLAN_DEVICE_AP_NAME "wlan1"
-#define RT_WLAN_SSID_MAX_LENGTH 32
-#define RT_WLAN_PASSWORD_MAX_LENGTH 32
-#define RT_WLAN_DEV_EVENT_NUM 2
-#define RT_WLAN_MANAGE_ENABLE
-#define RT_WLAN_SCAN_WAIT_MS 10000
-#define RT_WLAN_CONNECT_WAIT_MS 10000
-#define RT_WLAN_SCAN_SORT
-#define RT_WLAN_MSH_CMD_ENABLE
-#define RT_WLAN_AUTO_CONNECT_ENABLE
-#define AUTO_CONNECTION_PERIOD_MS 2000
-#define RT_WLAN_CFG_ENABLE
-#define RT_WLAN_CFG_INFO_MAX 3
-#define RT_WLAN_PROT_ENABLE
-#define RT_WLAN_PROT_NAME_LEN 8
-#define RT_WLAN_PROT_MAX 2
-#define RT_WLAN_DEFAULT_PROT "lwip"
-#define RT_WLAN_PROT_LWIP_ENABLE
-#define RT_WLAN_PROT_LWIP_NAME "lwip"
-#define RT_WLAN_WORK_THREAD_ENABLE
-#define RT_WLAN_WORKQUEUE_THREAD_NAME "wlan"
-#define RT_WLAN_WORKQUEUE_THREAD_SIZE 2048
-#define RT_WLAN_WORKQUEUE_THREAD_PRIO 15
 
 /* Using USB */
 
@@ -154,7 +129,6 @@
 #define RT_USING_POSIX_FS
 #define RT_USING_POSIX_POLL
 #define RT_USING_POSIX_SELECT
-#define RT_USING_POSIX_SOCKET
 
 /* Interprocess Communication (IPC) */
 
@@ -164,62 +138,6 @@
 
 /* Network */
 
-#define RT_USING_SAL
-#define SAL_INTERNET_CHECK
-
-/* Docking with protocol stacks */
-
-#define SAL_USING_LWIP
-#define SAL_USING_POSIX
-#define RT_USING_NETDEV
-#define NETDEV_USING_IFCONFIG
-#define NETDEV_USING_PING
-#define NETDEV_USING_NETSTAT
-#define NETDEV_USING_AUTO_DEFAULT
-#define NETDEV_IPV4 1
-#define NETDEV_IPV6 0
-#define RT_USING_LWIP
-#define RT_USING_LWIP203
-#define RT_USING_LWIP_VER_NUM 0x20003
-#define RT_LWIP_MEM_ALIGNMENT 4
-#define RT_LWIP_IGMP
-#define RT_LWIP_ICMP
-#define RT_LWIP_DNS
-#define RT_LWIP_DHCP
-#define IP_SOF_BROADCAST 1
-#define IP_SOF_BROADCAST_RECV 1
-
-/* Static IPv4 Address */
-
-#define RT_LWIP_IPADDR "192.168.1.30"
-#define RT_LWIP_GWADDR "192.168.1.1"
-#define RT_LWIP_MSKADDR "255.255.255.0"
-#define RT_LWIP_UDP
-#define RT_LWIP_TCP
-#define RT_LWIP_RAW
-#define RT_MEMP_NUM_NETCONN 8
-#define RT_LWIP_PBUF_NUM 16
-#define RT_LWIP_RAW_PCB_NUM 4
-#define RT_LWIP_UDP_PCB_NUM 4
-#define RT_LWIP_TCP_PCB_NUM 4
-#define RT_LWIP_TCP_SEG_NUM 40
-#define RT_LWIP_TCP_SND_BUF 8196
-#define RT_LWIP_TCP_WND 8196
-#define RT_LWIP_TCPTHREAD_PRIORITY 10
-#define RT_LWIP_TCPTHREAD_MBOX_SIZE 8
-#define RT_LWIP_TCPTHREAD_STACKSIZE 1024
-#define RT_LWIP_ETHTHREAD_PRIORITY 12
-#define RT_LWIP_ETHTHREAD_STACKSIZE 1024
-#define RT_LWIP_ETHTHREAD_MBOX_SIZE 8
-#define LWIP_NETIF_STATUS_CALLBACK 1
-#define LWIP_NETIF_LINK_CALLBACK 1
-#define SO_REUSE 1
-#define LWIP_SO_RCVTIMEO 1
-#define LWIP_SO_SNDTIMEO 1
-#define LWIP_SO_RCVBUF 1
-#define LWIP_SO_LINGER 0
-#define LWIP_NETIF_LOOPBACK 0
-#define RT_LWIP_USING_PING
 
 /* Utilities */
 
@@ -238,32 +156,15 @@
 
 /* Wiced WiFi */
 
-#define PKG_USING_RW007
-#define PKG_USING_RW007_V201
-#define RW007_USING_STM32_DRIVERS
-#define RW007_SPI_MAX_HZ 30000000
-#define RW007_SPI_BUS_NAME "spi2"
-#define RW007_CS_PIN 90
-#define RW007_BOOT0_PIN 29
-#define RW007_BOOT1_PIN 90
-#define RW007_INT_BUSY_PIN 107
-#define RW007_RST_PIN 111
-#define PKG_USING_NETUTILS
-#define PKG_NETUTILS_TFTP
-#define PKG_NETUTILS_IPERF
-#define PKG_NETUTILS_NETIO
-#define PKG_NETUTILS_NTP
-#define NTP_USING_AUTO_SYNC
-#define NTP_AUTO_SYNC_FIRST_DELAY 30
-#define NTP_AUTO_SYNC_PERIOD 3600
-#define NETUTILS_NTP_HOSTNAME "cn.ntp.org.cn"
-#define NETUTILS_NTP_HOSTNAME2 "ntp.rt-thread.org"
-#define NETUTILS_NTP_HOSTNAME3 "edu.ntp.org.cn"
-#define PKG_NETUTILS_TELNET
-#define PKG_NETUTILS_TCPDUMP
-#define PKG_NETUTILS_TCPDUMP_DBG
-#define PKG_USING_NETUTILS_V133
-#define PKG_NETUTILS_VER_NUM 0x10303
+
+/* CYW43012 WiFi */
+
+
+/* BL808 WiFi */
+
+
+/* CYW43439 WiFi */
+
 
 /* IoT Cloud */
 
@@ -309,13 +210,18 @@
 
 /* peripheral libraries and drivers */
 
+/* HAL & SDK Drivers */
+
+/* STM32 HAL & SDK Drivers */
+
+
+/* Kendryte SDK */
+
+
 /* sensors drivers */
 
 
 /* touch drivers */
-
-
-/* Kendryte SDK */
 
 
 /* AI packages */
@@ -333,8 +239,6 @@
 
 /* entertainment: terminal games and other interesting software packages */
 
-#define PKG_USING_OPTPARSE
-#define PKG_USING_OPTPARSE_V100
 
 /* Arduino libraries */
 
@@ -386,7 +290,6 @@
 #define BSP_USING_FLASH_FATFS
 #define BSP_USING_FAL
 #define BSP_USING_BOOTLOADER
-#define BSP_USING_RW007_WLAN
 
 /* On-chip Peripheral Drivers */
 
