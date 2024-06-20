8628114296f3e56462bc05203ad88c6b1eed8668
PeterJhon <peter_john@qq.com>
2024-06-20 17:42:09 +0800

    [libraries][HAL_Drviers][drv_usart]fix BUG

--- libraries/HAL_Drivers/drv_usart.c
+++ libraries/HAL_Drivers/drv_usart.c
@@ -489,7 +489,14 @@ static void dma_isr(struct rt_serial_device *serial)
         }
         else
         {
-            recv_len = serial->config.bufsz - uart->dma_rx.last_index + recv_total_index;
+            if(recv_total_index > uart->dma_rx.last_index)
+            {
+                recv_len = recv_total_index - uart->dma_rx.last_index;
+            }
+            else
+            {
+                recv_len = serial->config.bufsz - uart->dma_rx.last_index + recv_total_index;
+            }
         }
         uart->dma_rx.last_index = recv_total_index;
         rt_hw_interrupt_enable(level);
